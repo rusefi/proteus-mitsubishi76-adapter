4cf29c40183e88bfaea3bfc92df3ae430faa5b43
Vladimir Vinogradov <qwerty-off@mail.ru>
2021-12-30 00:16:25 +0300

    Fab preview

---
+++ step3_create_proteus-mitsubishi76-adapter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd hellen-one
+sh bin/create_board_with_prefix.sh "proteus" "../proteus-mitsubishi76-adapter" "-mitsubishi76-adapter" "a" "bom_replace_proteus-mitsubishi76-adapter-a.csv" "80,0"
